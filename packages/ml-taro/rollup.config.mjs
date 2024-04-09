abbc3987c2a7307e0460326bed929a0be031edfe
rockywu 吴佳雷 <wjl19890427@hotmail.com>
2024-04-09 09:38:44 +0800

    添加ml-taro

---
+++ packages/ml-taro/rollup.config.mjs
@@ -0,0 +1,23 @@
+// rollup.config.js
+
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+const name = "index"
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: `dist/${name}.cjs.js`,
+      format: 'cjs' // CommonJS 格式
+    },
+    {
+      file: `dist/${name}.esm.js`, // ES模块格式输出文件
+      format: 'es',
+    },
+  ],
+  plugins: [
+    resolve(), // 解析导入的第三方模块
+    typescript({ tsconfig: './tsconfig.json' }) // 使用 TypeScript 插件
+  ]
+};
