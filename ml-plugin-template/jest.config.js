4a8e71a9308708e260445aa4801ae1d0b605869c
rockywu 吴佳雷 <wjl19890427@hotmail.com>
2024-04-23 10:59:19 +0800

    修改代码

--- ml-plugin-template/jest.config.js
+++ ml-plugin-template/jest.config.js
@@ -2,9 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: [
-    "**/__tests__/**/*.(spec|test).[tj]s"
+    '**/__tests__/**/*.(spec|test).[tj]s'
   ],
   testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-  ],
+    '<rootDir>/node_modules/'
+  ]
 };
